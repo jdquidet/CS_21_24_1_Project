05d368d05f94052d17db2f7eabecd4594f983217
Jan Albert <jdquidet@up.edu.ph>
2024-11-26 00:50:11 +0800

    Update fivetwelve.asm

--- fivetwelve.asm
+++ fivetwelve.asm
@@ -14,7 +14,14 @@ NUMBER_128:	.asciiz "128"
 NUMBER_256:	.asciiz "256"
 NUMBER_512:	.asciiz "512"
 NEWLINE:	.asciiz "\n"
-ENTER_MOVE:	.asciiz "Enter a move (W=Up, A=Left, S=Down, D=Right, X=Exit):\n"
+WIN:		.asciiz "Congratulations! You have reached the 512 tile!"
+LOSE: 		.asciiz "Game Over."
+ENTER_MOVE:	.asciiz "Enter a move (W=Up, A=Left, S=Down, D=Right, X=Exit, 3=Disable RNG, 4=Enable RNG):\n"
+INVALID_MOVE:	.asciiz "Invalid move.\n"
+RNG_DISABLED:	.asciiz "RNG Disabled.\n"
+RNG_ENABLED:	.asciiz "RNG Enabled.\n"
+MOVE:		.word	
+STORED_GRID:	.word 	0, 0, 0, 0, 0, 0, 0, 0, 0
 
 .text
 main:
@@ -28,43 +35,62 @@ main:
 	
 	beq 	$t0, 1, input_random
 	beq 	$t0, 2, input_defined
+main_loop:
+	jal	print_grid
+	jal	is_win
+	jal	is_lose
+input_move:	
+	li 	$v0, 4 		
+    	la 	$a0, ENTER_MOVE
+    	syscall
 
-input_random:				# NEW GAME
+	li	$v0, 8
+	la	$a0, MOVE
+	li	$a1, 100
+	syscall
+	
+	lw	$t0, MOVE
+	beq	$t0, 0xa58, game_over	# X = game over
+	beq	$t0, 0xa57, move_up	# W = move up
+	beq	$t0, 0xa41, move_left	# A = move left
+	beq	$t0, 0xa53, move_down	# S = move down
+	beq	$t0, 0xa44, move_right	# D = move right
+	beq	$t0, 0xa33, rng_disable	# 3 = disable RNG
+	beq	$t0, 0xa34, rng_enable	# 4 = enable RNG
+	
+	li 	$v0, 4 		
+    	la 	$a0, INVALID_MOVE
+    	syscall
+	j	input_move
+
+input_random:				# NEW GAME 1
 	li 	$t0, 0
-    	
+	
     	jal 	zero_or_two
     	move 	$t1, $v0   
-    	
     	jal 	zero_or_two
-    	move 	$t2, $v0
-    	
+    	move 	$t2, $v0    	
     	jal 	zero_or_two
-    	move 	$t3, $v0
-    	
+    	move 	$t3, $v0    	
     	jal 	zero_or_two
-    	move 	$t4, $v0
-    	
+    	move 	$t4, $v0    	
     	jal 	zero_or_two
-    	move 	$t5, $v0
-    	
+    	move 	$t5, $v0    	
     	jal 	zero_or_two
-    	move 	$t6, $v0
-    	
+    	move 	$t6, $v0    	
     	jal 	zero_or_two
-    	move 	$t7, $v0
-    	
+    	move 	$t7, $v0    	
     	jal 	zero_or_two
-    	move 	$t8, $v0
-    	
+    	move 	$t8, $v0    	
     	jal 	zero_or_two
     	move 	$t9, $v0
     	
-    	j 	print_grid		# proceeds to grid printing
+    	j 	main_loop		# proceeds to game loop
 
 	# Used Functions for randomized zero or two #
 zero_or_two:
 	li 	$a1, 2 
-    	li 	$v0, 42  
+    	li 	$v0, 42			# Randomizer syscall  
     	syscall
 
 	beq 	$a0, 0, zero
@@ -81,7 +107,7 @@ two:
 	jr 	$ra
 	# End #
 
-input_defined:				# START FROM A STATE
+input_defined:				# NEW GAME 2
 	li 	$v0, 4 	
     	la 	$a0, BOARD_CONFIG
     	syscall
@@ -122,9 +148,14 @@ input_defined:				# START FROM A STATE
 	syscall
 	move 	$t9, $v0
 	
-	j 	print_grid		# proceeds to grid printing
+	j 	main_loop		# proceeds to game loop
 
 print_grid:				# GRID PRINTING
+	##### preamble #####
+	addi	$sp, $sp, -4
+	sw	$ra, 0($sp)
+	##### preamble #####
+
 	jal	print_line
 	
 	jal	print_vertical		# Row 1
@@ -168,12 +199,12 @@ print_grid:				# GRID PRINTING
 	jal 	print_newline
 	
 	jal	print_line
-	
-	li 	$v0, 4 		
-    	la 	$a0, ENTER_MOVE
-    	syscall
-    	
-    	j 	user_move
+   
+   	##### end #####
+   	lw	$ra, 0($sp)
+   	addi	$sp, $sp, 4
+   	##### end #####
+    	jr 	$ra
 
 	# Used Functions for grid printing #
 print_line:
@@ -189,16 +220,20 @@ print_vertical:
 	
 	jr	$ra
 print_integer:
-	addi	$sp, $sp, -8
+	##### preamble #####
+	addi	$sp, $sp, -4
 	sw	$ra, 0($sp)
+	##### preamble #####
 		
 	jal	get_integer
 	move	$a0, $v0
 	li	$v0, 4
 	syscall
 	
+	##### end #####
 	lw	$ra, 0($sp)
-	addi	$sp, $sp, 8
+	addi	$sp, $sp, 4
+	##### end #####
 	jr $ra
 get_integer:
 	beq	$a0, 0, print_0
@@ -250,6 +285,286 @@ print_newline:
 	jr $ra
 	# End #
 	
-user_move:
+is_win:
+	beq 	$t1, 512, win
+	beq 	$t2, 512, win
+	beq 	$t3, 512, win
+	beq 	$t4, 512, win
+	beq 	$t5, 512, win
+	beq 	$t6, 512, win
+	beq 	$t7, 512, win
+	beq 	$t8, 512, win
+	beq 	$t9, 512, win
 	
+	jr	$ra
+win:
+	li 	$v0, 4 		
+    	la 	$a0, WIN
+    	syscall
+    	
+    	j 	exit
+    	
+is_lose:
+	beqz	$t1, False		# Check if there's an empty tile
+	beqz	$t2, False
+	beqz	$t3, False
+	beqz	$t4, False
+	beqz	$t5, False
+	beqz	$t6, False
+	beqz	$t7, False
+	beqz	$t8, False
+	beqz	$t9, False
+
+	beq	$t1, $t2, False		# Check if adjacent tiles can combine
+	beq	$t1, $t4, False
+	beq	$t2, $t3, False
+	beq	$t2, $t5, False
+	beq	$t3, $t6, False
+	beq	$t4, $t5, False
+	beq	$t4, $t7, False
+	beq	$t5, $t6, False
+	beq	$t5, $t8, False
+	beq	$t6, $t9, False
+	beq	$t7, $t8, False
+	beq	$t8, $t9, False
+game_over:	
+	li 	$v0, 4 		
+    	la 	$a0, LOSE
+    	syscall
+    	
+    	j exit
+False:
+	jr	$ra
 	
+exit:
+	li 	$v0, 10
+	syscall
+	
+move_up:
+	move	$a0, $t1
+	move	$a1, $t4
+	move	$a2, $t7
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t1, $a0
+	move	$t4, $a1
+	move	$t7, $a2
+	
+	move	$a0, $t2
+	move	$a1, $t5
+	move	$a2, $t8
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t2, $a0
+	move	$t5, $a1
+	move	$t8, $a2
+	
+	move	$a0, $t3
+	move	$a1, $t6
+	move	$a2, $t9
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t3, $a0
+	move	$t6, $a1
+	move	$t9, $a2
+	
+	j	add_random_tile
+	
+move_left:
+	move	$a0, $t1
+	move	$a1, $t2
+	move	$a2, $t3
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t1, $a0
+	move	$t2, $a1
+	move	$t3, $a2
+	
+	move	$a0, $t4
+	move	$a1, $t5
+	move	$a2, $t6
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t4, $a0
+	move	$t5, $a1
+	move	$t6, $a2
+	
+	move	$a0, $t7
+	move	$a1, $t8
+	move	$a2, $t9
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t7, $a0
+	move	$t8, $a1
+	move	$t9, $a2
+	
+	j	add_random_tile
+	
+move_down:
+	move	$a0, $t7
+	move	$a1, $t4
+	move	$a2, $t1
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t7, $a0
+	move	$t4, $a1
+	move	$t1, $a2
+	
+	move	$a0, $t8
+	move	$a1, $t5
+	move	$a2, $t2
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t8, $a0
+	move	$t5, $a1
+	move	$t2, $a2
+	
+	move	$a0, $t9
+	move	$a1, $t6
+	move	$a2, $t3
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t9, $a0
+	move	$t6, $a1
+	move	$t3, $a2
+	
+	j	add_random_tile
+
+move_right:
+	move	$a0, $t3
+	move	$a1, $t2
+	move	$a2, $t1
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t3, $a0
+	move	$t2, $a1
+	move	$t1, $a2
+	
+	move	$a0, $t6
+	move	$a1, $t5
+	move	$a2, $t4
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t6, $a0
+	move	$t5, $a1
+	move	$t4, $a2
+	
+	move	$a0, $t9
+	move	$a1, $t8
+	move	$a2, $t7
+	jal 	compress
+	jal	merge
+	jal 	compress
+	move	$t9, $a0
+	move	$t8, $a1
+	move	$t7, $a2
+	
+	j	add_random_tile
+
+compress:
+	bnez	$a1, compress_1
+	move	$a1, $a2
+	li	$a2, 0
+compress_1:
+	bnez	$a0, compress_2
+	move	$a0, $a1
+	li	$a1, 0
+compress_2:
+	bnez	$a1, compress_end
+	move	$a1, $a2
+	li	$a2, 0
+compress_end:
+	jr	$ra
+
+merge:
+	bne	$a0, $a1, merge_1
+	add	$a0, $a0, $a1
+	li	$a1, 0
+merge_1:
+	bne	$a1, $a2, merge_end
+	add	$a1, $a1, $a2
+	li	$a2, 0
+merge_end:
+	jr 	$ra
+			
+add_random_tile:
+	beq	$s0, 3, main_loop
+
+	li 	$a1, 9 
+    	li 	$v0, 42			# Randomizer syscall  
+    	syscall
+	
+	beq	$a0, 0, tile_t1
+	beq	$a0, 1, tile_t2
+	beq	$a0, 2,	tile_t3
+	beq	$a0, 3,	tile_t4
+	beq	$a0, 4,	tile_t5
+	beq	$a0, 5,	tile_t6
+	beq	$a0, 6,	tile_t7
+	beq	$a0, 7,	tile_t8
+	beq	$a0, 8,	tile_t9
+	
+tile_t1:
+	bnez	$t1, add_random_tile
+	li	$t1, 2
+	j	main_loop
+tile_t2:
+	bnez	$t2, add_random_tile
+	li	$t2, 2
+	j	main_loop
+tile_t3:
+	bnez	$t3, add_random_tile
+	li	$t3, 2
+	j	main_loop
+tile_t4:
+	bnez	$t4, add_random_tile
+	li	$t4, 2
+	j	main_loop
+tile_t5:
+	bnez	$t5, add_random_tile
+	li	$t5, 2
+	j	main_loop
+tile_t6:
+	bnez	$t6, add_random_tile
+	li	$t6, 2
+	j	main_loop
+tile_t7:
+	bnez	$t7, add_random_tile
+	li	$t7, 2
+	j	main_loop
+tile_t8:
+	bnez	$t8, add_random_tile
+	li	$t8, 2
+	j	main_loop
+tile_t9:
+	bnez	$t9, add_random_tile
+	li	$t9, 2
+	j	main_loop
+																
+rng_disable:
+	li	$s0, 3	
+	
+	li 	$v0, 4 		
+    	la 	$a0, RNG_DISABLED
+    	syscall
+    	
+    	j	input_move
+rng_enable:	
+	li	$s0, 4	
+	
+	li 	$v0, 4 		
+    	la 	$a0, RNG_ENABLED
+    	syscall
+    	
+    	j	input_move
